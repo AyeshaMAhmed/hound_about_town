10d9567f84c4fcd79f7d0fddb049321f09399110
Ayesha Ahmed <ayesha.m.ahmed@gmail.com>
2017-06-29 09:49:50 -0700

    fixed 404 error

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
 resources :users
 
+get "/" => "sessions#new"
+
 get "/login" => "sessions#new"
 post "/login" => "sessions#create"
 get "/logout" => "sessions#destroy"
